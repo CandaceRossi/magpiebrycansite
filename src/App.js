22a4e9dbcd60c41b30b4283f36e84950a1528a96
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2019-09-25 21:00:27 -0400

    figured out bugs with prop passing for products and working out routes for additional product info

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
 import NavTagg from "./components/Navtagg";
+import ItemList from "./components/ItemList";
+import Item from "./components/Item";
+import data from "./data";
 
-const App = props => {
+const App = () => {
+  const [product] = useState(data);
   return (
     <div className="App">
       <h1 className="store-header">Magpie Designs</h1>
       <div className="component-links">
         <NavTagg />
       </div>
+      <Route
+        exact
+        path="/ItemList"
+        render={props => <ItemList items={product} {...props} />}
+      />
+      <Route
+        exact
+        path="/ItemList/:id"
+        render={props => <Item items={product} {...props} />}
+      />
     </div>
   );
 };
