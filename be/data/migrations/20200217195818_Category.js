0750cb82a5396b1a87c4aeda3ce9ee39b867c9a3
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2020-02-18 18:23:36 -0500

    setting up migrations

--- be/data/migrations/20200217195818_Category.js
+++ be/data/migrations/20200217195818_Category.js
@@ -1,6 +1,16 @@
 
 exports.up = function (knex) {
+    return knex.schema.createTable('category', col => {
+        col.increments('category_id');
 
+        col.varchar('category_name', 255)
+            .notNullable()
+            .unique();
+        col.text('description')
+            .notNullable()
+        col.timestamp('create_at').defaultTo(knex.fn.now())
+        col.timestamp('updated_at').defaultTo(knex.fn.now())
+    })
 };
 
 exports.down = function (knex) {
--- be/data/migrations/20200217195910_Customer.js
+++ be/data/migrations/20200217195910_Customer.js
@@ -1,6 +1,24 @@
 
 exports.up = function (knex) {
-
+    return knex.schema.createTable('customer', col => {
+        col.increments('customer_id');
+        col.varchar('company_name', 255)
+        col.varchar('contact_name', 255)
+            .notNullable();
+        col.varchar('contact_title', 255)
+        col.varchar('address', 255)
+            .notNullable()
+        col.varchar('city', 255)
+            .notNullable()
+        col.varchar('region', 255)
+            .notNullable()
+        col.varchar('postal_code', 255)
+            .notNullable()
+        col.varchar('', 255)
+            .notNullable()
+        col.timestamp('create_at').defaultTo(knex.fn.now())
+        col.timestamp('updated_at').defaultTo(knex.fn.now())
+    })
 };
 
 exports.down = function (knex) {
