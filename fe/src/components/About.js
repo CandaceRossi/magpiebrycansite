a96ebe72faee7203df70680dfbd559ed1e36c9b3
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2020-03-26 18:44:44 -0400

    resytled the nav bars and about component, added stock photos

--- fe/src/components/About.js
+++ fe/src/components/About.js
@@ -1,43 +1,34 @@
 import React, { Component } from "react";
+// import magpiepie from "./images/maglogo/1x/magpiepie.png";
+
 
 class About extends Component {
   render() {
     return (
+      // <div> <img src={magpiepie} height="75" alt="" /></div>
       <div className="sign-design">
+        {/* <div class="sidebar-box"> */}
+        <h3 className="header-style">GET TO KNOW</h3>
+        <h4 style={{ textAlign: 'center', color: '#683B2B', fontFamily: 'Comic Sans MS, cursive, sans-serif' }}>our story</h4>
+        <p>
+          Magpie Designs is a family workshed and creative outsource for
+          impactful designs seen all over the world.
+          </p>
+        <p>
+          Our designs are unique and mimic style from cultures broad and far.
+          Many within our selection have strong bold and beautiful textiles
+          which offer a story of its historical liniage, to be remembered and
+          celebrated. to the designs
+          </p>
+        <p>
+          Magpie Designs is a family workshed and creative outsource for
+          impactful designs seen all over the world.
+          </p>
         <div class="sidebar-box">
-          <h3 className="header-style">About</h3>
-          <p>
-            Magpie Designs is a family workshed and creative outsource for
-            impactful designs seen all over the world.
-          </p>
-          <p>
-            Our designs are unique and mimic style from cultures broad and far.
-            Many within our selection have strong bold and beautiful textiles
-            which offer a story of its historical liniage, to be remembered and
-            celebrated. to the designs
-          </p>
-          <p>
-            Magpie Designs is a family workshed and creative outsource for
-            impactful designs seen all over the world.
-          </p>
-          <p>
-            Our designs are unique and mimic style from cultures brad and far.
-            Many within our selection have strong bold and beautiful textiles
-            which offer a story of its historical liniage, to be remembered and
-            celebrated. to the designs
-          </p>
-          <p>
-            Magpie Designs is a family workshed and creative outsource for
-            impactful designs seen all over the world.
-          </p>
-          <p>
-            Our designs are unique and mimic style from cultures brad and far.
-            Many within our selection have strong bold and beautiful textiles
-            which offer a story of its historical liniage, to be remembered and
-            celebrated. to the designs
-          </p>
-          <p class="read-more"><a href="#" class="button">Read More</a></p>
+          <p>A Journey of Love, Healing, and Awareness</p>
         </div>
+        {/* <p class="read-more"><a href="#" class="button">Read More</a></p> */}
+        {/* </div> */}
       </div>
     );
   }
